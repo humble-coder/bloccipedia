8b69648e8b55570daad6969c5f9aa7c04c75daa0
Mark A Brown <mark.philosophe@gmail.com>
2013-12-26 14:42:33 -0500

    updated association between users and wikis

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_many :wikis
+  has_and_belongs_to_many :wikis
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,4 @@
 class Wiki < ActiveRecord::Base
   attr_accessible :body, :title
-  has_many :users
-  belongs_to :user
+  has_and_belongs_to_many :users
 end
