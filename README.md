8442cd0496545984b2f78b74a787bf17e792f98e
Mark A Brown <mark.philosophe@gmail.com>
2013-12-26 11:58:35 -0500

    First commit and README update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bloccipedia
+
+A wiki-style application to further develop foundation programming and web development skills.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccipedia::Application.config.secret_token = '4c5eb320d548887b9f920f99c0c933cddac75c1000635d228d2eb8a4b54019097820a38a2930c9941813ba010ddb9f29fdb0f36dd259bdb7f76a711207f5825c'
