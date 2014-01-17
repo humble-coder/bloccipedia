035e4a9807d6700db3b4fba23885b3eff6323436
Mark A Brown <mark.philosophe@gmail.com>
2014-01-17 10:44:27 -0500

    removed role from users column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140107004326) do
+ActiveRecord::Schema.define(:version => 20140117153526) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(:version => 20140107004326) do
     t.string   "name"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "role"
     t.boolean  "premium"
   end
 
