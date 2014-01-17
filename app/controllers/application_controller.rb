87a64fca40c522530efd489068f71bfe333ec5d6
Mark A Brown <mark.philosophe@gmail.com>
2014-01-17 10:29:47 -0500

    removed superfluous spec files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  # after_filter :verify_authorized, :except => :index
-  # after_filter :verify_policy_scoped, :only => :index
   protect_from_forgery
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 
   def self.search(search)  
     if search  
-      where('name LIKE ?', "%#{search}%")  
+      where('name LIKE ?', "%#{search}%")
     else  
       scoped  
     end  
