64f4d5e99222b89c6c6cdc65dc79f87d4e0091f0
Mark A Brown <mark.philosophe@gmail.com>
2014-01-19 13:16:21 -0500

    updated app security via various secret tokens

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,15 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bloccipedia::Application.config.secret_token = '4c5eb320d548887b9f920f99c0c933cddac75c1000635d228d2eb8a4b54019097820a38a2930c9941813ba010ddb9f29fdb0f36dd259bdb7f76a711207f5825c'
+
+if Rails.env.development?
+	Bloccipedia::Application.config.secret_token = ENV['SECRET_TOKEN_DEV']
+end
+
+if Rails.env.test?
+	Bloccipedia::Application.config.secret_token = ENV['SECRET_TOKEN_TEST']
+end
+
+if Rails.env.production?
+	Bloccipedia::Application.config.secret_token = ENV['SECRET_TOKEN_PROD']
+end
