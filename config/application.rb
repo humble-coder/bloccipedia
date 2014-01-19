77f883eff3d5c52545d2e20a350023834bd6ea3f
Mark A Brown <mark.philosophe@gmail.com>
2014-01-19 13:48:54 -0500

    another attempt to update app security

--- config/application.rb
+++ config/application.rb
@@ -66,10 +66,10 @@ module Bloccipedia
     config.assets.version = '1.0'
 
     config.before_configuration do
-        env_file = File.join('config', 'local_env.yml')
+        env_file = 'config/local_env.yml'
         YAML.load(File.open(env_file)).each do |key, value|
             ENV[key.to_s] = value
-        end if File.exists?(env_file)
+        end
     end
 
     config.assets.initialize_on_precompile = false
