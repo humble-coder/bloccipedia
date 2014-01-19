5d0a6f21ede6a135e8c78961980d50ba6b1a05c7
Mark A Brown <mark.philosophe@gmail.com>
2014-01-19 14:56:26 -0500

    updated app security via various secret tokens

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,7 @@ module Bloccipedia
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # For security purposes
     config.before_configuration do
         env_file = File.join(Rails.root, 'config', 'local_env.yml')
         YAML.load(File.open(env_file)).each do |key, value|
